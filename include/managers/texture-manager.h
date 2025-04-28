74af9c75c442248ea09257edceef75ebeffaee13
qiekn <qiekn@kimidayo.cn>
2025-04-28 23:12:02 +0800

    feat: texture manager

---
+++ include/managers/texture-manager.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <raylib.h>
+#include <filesystem>
+#include <unordered_map>
+
+class TextureManager {
+public:
+  static TextureManager& Instance() {
+    static TextureManager* instance = new TextureManager();
+    return *instance;
+  }
+
+  virtual ~TextureManager() { UnloadAll(); }
+
+  /**
+   * @brief get texture, if not loaded, load it.
+   *
+   * @param path
+   * @return reference of texture 2d
+   */
+  Texture2D& GetTexture(const std::filesystem::path& path);
+
+  void UnloadAll();
+
+private:
+  std::unordered_map<std::filesystem::path, Texture2D> textures_;
+
+  TextureManager() {}
+
+  /** @brief used this texture when loading texture failed */
+  Texture2D CreateDefaultTexture();
+};
---
+++ src/managers/texture-manager.cpp
@@ -0,0 +1,30 @@
+#include "managers/texture-manager.h"
+#include <raylib.h>
+#include <filesystem>
+
+Texture2D& TextureManager::GetTexture(const std::filesystem::path& path) {
+  auto it = textures_.find(path);
+  if (it == textures_.end()) {
+    Texture2D texture = LoadTexture(path.c_str());
+    if (texture.id == 0) {
+      texture = CreateDefaultTexture();
+    }
+    textures_[path] = texture;
+    return textures_[path];
+  }
+  return it->second;
+}
+
+void TextureManager::UnloadAll() {
+  for (const auto& pair : textures_) {
+    UnloadTexture(pair.second);
+  }
+  textures_.clear();
+}
+
+Texture2D TextureManager::CreateDefaultTexture() {
+  Image image = GenImageColor(32, 32, MAGENTA);
+  Texture2D texture = LoadTextureFromImage(image);
+  UnloadImage(image);
+  return texture;
+}
