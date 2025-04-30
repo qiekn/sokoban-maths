231fe4dab0f9ba2aeff5b0f8bcd3e0fd1c936e9a
qiekn <qiekn@kimidayo.cn>
2025-04-30 17:38:08 +0800

    update: vector2int

--- include/types.h
+++ include/types.h
@@ -1,12 +1,36 @@
 #pragma once
 
-#include "constants.h"
 #include "entt.h"
-
-using Registry = entt::registry;
-using Map = std::unordered_map<std::pair<int, int>, entt::entity, PairHash>;
-using Entity = entt::entity;
 
 struct Vector2Int {
   int x, y;
+
+  Vector2Int() : x(0), y(0) {}
+
+  Vector2Int(int _x, int _y) : x(_x), y(_y) {}
+
+  Vector2Int operator+(const Vector2Int& rhs) const { return Vector2Int(x + rhs.x, y + rhs.y); }
+
+  Vector2Int operator-(const Vector2Int& rhs) const { return Vector2Int(x - rhs.x, y - rhs.y); }
+
+  Vector2Int operator*(int rhs) const { return Vector2Int(x * rhs, y * rhs); }
+
+  Vector2Int operator/(int rhs) const { return Vector2Int(x / rhs, y / rhs); }
+
+  Vector2Int& operator+=(const Vector2Int& rhs) {
+    x += rhs.x;
+    y += rhs.y;
+    return *this;
+  }
+
+  Vector2Int& operator-=(const Vector2Int& rhs) {
+    x -= rhs.x;
+    y -= rhs.y;
+    return *this;
+  }
 };
+
+using Registry = entt::registry;
+using Entity = entt::entity;
+
+using Map = std::unordered_map<Vector2Int, Entity>;
