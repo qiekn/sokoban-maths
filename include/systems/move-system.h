0f2806b5403604e42a5fe4413b3d93d8db1e1c8d
qiekn <qiekn@kimidayo.cn>
2025-05-02 00:44:37 +0800

    feat: move system push number to operator
    now we can push a number to a math operator box. Then it will
    overlap(number and operator have same position), we are going to fix
    that use a new system (MathSystem)

--- include/systems/move-system.h
+++ include/systems/move-system.h
@@ -13,4 +13,14 @@ public:
 
 private:
   Registry& registry_;
+  Map mov_map_;
+  Map immov_map_;
+  Map operator_map_;
+
+  void InitMaps();
+
+  bool TryMove(MoveBuffer& to_move, Entity entity, Vector2Int dir, int count);
+
+  template <typename Component>
+  void UpdateMap(Map& map);
 };
--- src/systems/move-system.cpp
+++ src/systems/move-system.cpp
@@ -10,36 +10,47 @@
  * @param direction
  * @param count (how many items can be move at once)
  */
-bool TryMove(Registry& registry, Map& mov, Map& immov, MoveBuffer& to_move,
-             Entity entity, Vector2Int dir, int count) {
+bool MoveSystem::TryMove(MoveBuffer& to_move, Entity entity, Vector2Int dir,
+                         int push_ability) {
   // get player original position and destination
-  auto& pos = registry.get<Position>(entity);
+  auto& pos = registry_.get<Position>(entity);
   Vector2Int dest = pos + dir;
+  TraceLog(LOG_DEBUG, "TryMove: %d %d, dir: %d %d", pos.x, pos.y, dir.x, dir.y);
+  to_move.emplace_back(entity, dir);  // register self
+
+  // magic move
+  // push number to operator box
+  bool src_is_number = registry_.all_of<Number>(entity);
+  bool dest_is_operator = operator_map_.find(dest) != operator_map_.end();
+  if (src_is_number && dest_is_operator) {
+    return true;
+  }
+
+  // out of map
+  if (dest.x < 0 || dest.x >= kCols || dest.y < 0 || dest.y >= kRows) {
+    TraceLog(LOG_DEBUG, "TryMove: out of map");
+    return false;
+  }
 
   // hit obstacle
-  if (immov.find(dest) != immov.end()) {
-    TraceLog(LOG_DEBUG, "TryMove: (%d %d) Hit Obstacle", pos.x + 1, pos.y + 1);
+  if (immov_map_.find(dest) != immov_map_.end()) {
+    TraceLog(LOG_DEBUG, "TryMove: hit obstacle");
     return false;
   }
 
-  // check map border
-  if (dest.x < 0 || dest.x >= kCols || dest.y < 0 || dest.y >= kRows) {
-    return false;
-  }
-
-  // register self
-  to_move.emplace_back(entity, dir);
-
-  // hit movable item, recur trymove other
-  if (mov.find(dest) != mov.end()) {
-    if (count >= 1) {
-      Entity other = mov[dest];
-      return TryMove(registry, mov, immov, to_move, other, dir, --count);
+  // hit movable
+  bool hit_movable = mov_map_.find(dest) != mov_map_.end();
+  if (hit_movable) {
+    if (push_ability > 0) {
+      TraceLog(LOG_DEBUG, "TryMove: hit movable");
+      Entity other = mov_map_[dest];
+      return TryMove(to_move, other, dir, --push_ability);
     }
-    // can't push bacause of no 'count' left
     return false;
   }
 
+  // hit nothing
+  TraceLog(LOG_DEBUG, "TryMove: hit nothing");
   return true;
 }
 
@@ -51,22 +62,9 @@ bool TryMove(Registry& registry, Map& mov, Map& immov, MoveBuffer& to_move,
  *
  */
 MoveBuffer MoveSystem::Move(std::vector<Entity> entities, Vector2Int dir) {
+  InitMaps();
   MoveBuffer to_move;
-
-  // gather mappings
-  Map mov_map;    // movable boxes map
-  Map immov_map;  // immovable boxes map
-  auto mov_view = registry_.view<Position, Movable>();
-  for (auto e : mov_view) {
-    auto& pos = registry_.get<Position>(e);
-    mov_map[pos] = e;
-  }
-  auto immov_view = registry_.view<Position, Imovable>();
-  for (auto e : immov_view) {
-    auto& pos = registry_.get<Position>(e);
-    immov_map[pos] = e;
-  }
-
+  TraceLog(LOG_DEBUG, "MoveSystem: start");
   // check move for each entity
   for (const auto entity : entities) {
     // now this method only used by player entities
@@ -74,9 +72,27 @@ MoveBuffer MoveSystem::Move(std::vector<Entity> entities, Vector2Int dir) {
     if (!registry_.all_of<Player, Position>(entity)) {
       continue;
     }
-    if (!TryMove(registry_, mov_map, immov_map, to_move, entity, dir, 1)) {
+    if (!TryMove(to_move, entity, dir, 1)) {
+      TraceLog(LOG_DEBUG, "MoveSystem: failed");
       return {};
     }
   }
+  TraceLog(LOG_DEBUG, "MoveSystem: succeed");
   return to_move;
+}
+
+template <typename Component>
+void MoveSystem::UpdateMap(Map& map) {
+  map.clear();
+  auto view = registry_.view<Position, Component>();
+  for (auto e : view) {
+    const auto& pos = registry_.get<Position>(e);
+    map[pos] = e;
+  }
+}
+
+void MoveSystem::InitMaps() {
+  UpdateMap<Movable>(mov_map_);
+  UpdateMap<Imovable>(immov_map_);
+  UpdateMap<MathOperator>(operator_map_);
 }
